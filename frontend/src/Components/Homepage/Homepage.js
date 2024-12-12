4e39d3b8a41622fe1e0f751fbdba8e883f6f855f
konghui <kong.hui@northeastern.edu>
2024-12-11 21:36:49 -0800

    main page design

--- frontend/src/Components/Homepage/Homepage.js
+++ frontend/src/Components/Homepage/Homepage.js
@@ -3,6 +3,7 @@ import { registerUser, loginUser } from '../../api';
 import "./Homepage.css";
 import Logo from "../../assets/images/Catlover.jpg"
 import HomeImage from "../../assets/images/homepage.jpg"
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     const [credentials, setCredentials] = useState({ username: '', password: ''});
@@ -44,14 +45,20 @@ const Homepage = () => {
                     <h1 className='home_right_title'>Cat lovers are here</h1>
                     <h2 className='home_right_subtitle'>Join Catlover today!</h2>
                     <div className='home_right_buttons'>
-                        <button className='btn_secondary'>Sign up with phone or email</button>
-                        <p className='policies'>
+                        <button className='btn_secondary'>
+                            <Link to="/register">Sign up with email</Link>
+                        </button>
+                    <p className='policies'>
                             By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
-                        </p>
-                    </div>
+                    </p>
+                </div>
                     <div className='home_right_buttons'>
                         <h3>Already have an account?</h3>
-                        <button className='btn_primary'>Sign in</button>
+                        <button className='btn_secondary'>
+                            <Link to="/login">Sign in</Link>
+                        </button>
+                        {/* link to the home page */}
+                        <button className='btn_primary'>Browse as a visitor</button>
                     </div>
                 </div>
             </div>
